a2b1d2ca33f1a6818ede8298519927c555d3dda5
Keisuke Yoshigai <yoshigai_keisuke@retail-ai.jp>
2023-04-02 21:18:27 +0900

    Add MakeSomething2 func and benchmark of chapter 8(after improving)

--- chapter8/something/something.go
+++ chapter8/something/something.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 )
 
-func MakeSomething(n int) []string {
+func MakeSomething1(n int) []string {
 	var r []string
 	for i := 0; i < n; i++ {
 		r = append(r, fmt.Sprintf("%05d something", i))
+	}
+	return r
+}
+
+func MakeSomething2(n int) []string {
+	r := make([]string, n)
+
+	for i := 0; i < n; i++ {
+		r[i] = fmt.Sprintf("%05d something", i)
 	}
 	return r
 }
--- chapter8/something/something_test.go
+++ chapter8/something/something_test.go
@@ -6,9 +6,34 @@ import (
 	"github.com/k-yoshigai/go-programming-essence/chapter8/something"
 )
 
-func BenchmarkMakeSomething(b *testing.B) {
+func BenchmarkMakeSomething1(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = something.MakeSomething(1000)
+		_ = something.MakeSomething1(1000)
 	}
 }
+
+// Benchmark Result(BenchmarkMakeSomething1)
+// ❯ go test -benchmem -bench MakeSomething1
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/k-yoshigai/go-programming-essence/chapter8/something
+// BenchmarkMakeSomething1-8          14095             81212 ns/op           72390 B/op       1756 allocs/op
+// PASS
+// ok      github.com/k-yoshigai/go-programming-essence/chapter8/something 2.364s
+
+func BenchmarkMakeSomething2(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = something.MakeSomething2(1000)
+	}
+}
+
+// Benchmark Result(BenchmarkMakeSomething1)
+// ❯ go test -benchmem -bench MakeSomething2
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/k-yoshigai/go-programming-essence/chapter8/something
+// BenchmarkMakeSomething2-8          15102             78295 ns/op           38347 B/op       1745 allocs/op
+// PASS
+// ok      github.com/k-yoshigai/go-programming-essence/chapter8/something 1.897s
